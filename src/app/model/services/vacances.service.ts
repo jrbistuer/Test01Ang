10db5f0a3afa4646df910fd35e21ec9b815b8b8e
jrbistuer <jrbistuer@gmail.com>
2023-11-23 17:40:56 +0100

    sessió 2

--- src/app/model/services/vacances.service.ts
+++ src/app/model/services/vacances.service.ts
@@ -1,34 +1,25 @@
 import { Injectable } from '@angular/core';
+import { IVacanca } from '../models/vacanca';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VacancesService {
 
+  vacances: IVacanca[] = [];
+
   constructor() { }
 
-  getVacances() {
-    return [
-      {
-        id: 0,
-        nom: "Roma",
-        preu: 234,
-        pais: "Italia",
-        descripcio: "asdcasdc asd asd "
-      }, {
-        id: 1,
-        nom: "París",
-        preu: 23452,
-        pais: "França",
-        descripcio: "ergtertghdrthbsd asd "
-      }, {
-        id: 0,
-        nom: "Berlin",
-        preu: 5435,
-        pais: "Alemanya",
-        descripcio: "asdrthbdrthbdf v  asd asd "
-      }
-    ];
+  getVacances(): IVacanca[] {
+    return this.vacances;
   }
 
+  setVacances(vacanca: IVacanca): void {
+    this.vacances.push(vacanca);
+  }
+
+  getVacancesById(i: number): IVacanca {
+    return this.vacances[i];
+  }
+  
 }
--- src/app/pages/vacances/vacances.component.ts
+++ src/app/pages/vacances/vacances.component.ts
@@ -2,24 +2,50 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { VacancesService } from '../../model/services/vacances.service';
+import { IVacanca } from '../../model/models/vacanca';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-vacances',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, ReactiveFormsModule],
   templateUrl: './vacances.component.html',
   styleUrl: './vacances.component.scss'
 })
 export class VacancesComponent implements OnInit {
 
-  vacances: any;
+  vacances: IVacanca[] = [];
+  vacancaForm!: FormGroup;
+  vacancaSelected: IVacanca = {} as IVacanca;
 
-  constructor(private vacancesService: VacancesService) {
-
+  constructor(private vacancesService: VacancesService,
+    private formBuilder: FormBuilder) {
   }
 
   ngOnInit(): void {
     this.vacances = this.vacancesService.getVacances();
+    this.initForm();
+  }
+
+  initForm() {
+    this.vacancaForm = this.formBuilder.group({
+      nom: [{value: '', disabled: false}, [Validators.required, Validators.minLength(5)]],
+      preu: [{value: '', disabled: false}, Validators.required],
+      pais: [{value: '', disabled: false}, Validators.required],
+      descripcio: [{value: '', disabled: false}]
+    })
+  }
+
+  guardarVacanca() {
+    if (this.vacancaForm.valid) {
+      this.vacancesService.setVacances(this.vacancaForm.value as IVacanca);
+      this.vacancesService.getVacances();
+    }
+    console.log(this.vacancaForm);
+  }
+
+  showVacanca($index: number) {
+    this.vacancaSelected = this.vacancesService.getVacancesById($index);
   }
 
 }
